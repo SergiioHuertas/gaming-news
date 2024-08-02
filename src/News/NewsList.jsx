cd76d7f97b98d9c9a4713c38bc9492446e495bc8
Sergio <ser_g@hotmail.es>
2024-08-02 12:34:08 +0200

    extract api key to .env and add pagination

--- src/News/NewsList.jsx
+++ src/News/NewsList.jsx
@@ -1,40 +1,56 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Constants from '../common/const';
+import { format } from 'date-fns';
+import { es } from 'date-fns/locale'
 
 export const NewsList = () => {
     const [articles, setArticles] = useState([]);
+    const [results, setResults] = useState(Constants.resultsPerPage);
+    const [totalResults, setTotalResults] = useState(0);
 
     const fetchArticles = async () => {
         try {
-            const response = await axios.get('https://newsapi.org/v2/everything?sortBy=publishedAt', {
+            const response = await axios.get(`${Constants.API_URL}?sortBy=publishedAt&pageSize=${results}`, {
                 params: {
-                    q: 'videojuegos',
-                    language: 'es',
-                    apiKey: '29c7462586634961818d175360e6cfdc'
+                    q: Constants.searchFor,
+                    language: Constants.language,
+                    apiKey: import.meta.env.VITE_API_URL
                 }
             });
             setArticles(response.data.articles);
+            setTotalResults(response.data.totalResults);
         } catch (error) {
             console.error('Error fetching the news articles:', error);
         }
     };
 
     useEffect(() => {
-        fetchArticles().then(console.log('articles', articles));
-    }, []);
+        fetchArticles();
+    }, [results]);
 
     return (
         <div>
             <h1>Noticias de Videojuegos</h1>
             <ul>
-                {articles.map((article, index) => (
+                {articles.map((article, index) => { 
+                    return (
                     <div className={"card-new"} onClick={() => window.open(article.url)} key={index}>
+                        <div className='tags-menu'>
+                            <div className='tag'>{article.source?.name}</div>
+                            <div className='tag'>{format(new Date(article.publishedAt), "dd/MM/yyyy", {locale: es})}</div>
+                        </div>
                         <h2 className={"new-title"}>{article.title}</h2>
                         <img className={"new-img"} src={article.urlToImage} alt={article.title} width={500} />
                         <p className={"new-description"}>{article.description}</p>
                     </div>
-                ))}
+                )}
+                )}
             </ul>
+
+            {results < totalResults && 
+              <button onClick={() => setResults(results + Constants.resultsPerPage)}>Cargar más</button>
+            }
         </div>
     );
 };
---
+++ src/common/const.js
@@ -0,0 +1,8 @@
+const Constants = {};
+
+Constants.API_URL = 'https://newsapi.org/v2/everything';
+Constants.resultsPerPage = 10;
+Constants.searchFor = 'videojuegos';
+Constants.language = 'es';
+
+export default Constants;
